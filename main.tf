211fcd8235af506ad1c663f8f0dab52e9ae5dbb4
vinay <sterlingvinay01@gmail.com>
2024-08-02 12:24:25 -0400

    deployment file

--- main.tf
+++ main.tf
@@ -80,6 +80,6 @@ resource "aws_lb_listener" "app" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.blue.arn
+    target_group_arn = aws_lb_target_group.green.arn
   }
 }
